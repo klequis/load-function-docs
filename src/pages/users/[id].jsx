eb7ebbe8c9f1f0516695f64d274920e5831c7ec0
klequis <klequis@gmail.com>
2024-04-05 10:52:15 -0700

    init

---
+++ src/pages/users/[id].jsx
@@ -0,0 +1,14 @@
+import { createAsync } from "@solidjs/router";
+import { getUser } from "./[id].data";
+
+export default function Users(props) {
+  const user = createAsync(() => getUser(props.params.id));
+  return (
+    <>
+      <h1>User</h1>
+      <div>
+        <pre>{JSON.stringify(user(), null, 2)}</pre>
+      </div>
+    </>
+  )
+}
